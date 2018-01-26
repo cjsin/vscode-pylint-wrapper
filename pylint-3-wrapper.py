5f093ac34a8b865fcaf3e6f5112e959cd7d43ab9
cjsin <19607446+cjsin@users.noreply.github.com>
2018-01-26 18:01:45 +0800

    Update pylint-3-wrapper.py

--- pylint-3-wrapper.py
+++ pylint-3-wrapper.py
@@ -1,29 +1,29 @@
 #!/usr/bin/python3
-MAX_LIFE_SECONDS=120
-MAXFD = 1024
-REAL_EXEC="/usr/bin/pylint-3"
-
 import os
 import resource
 import subprocess
 import sys
 
+MAX_LIFE_SECONDS=120
+REAL_EXEC="/usr/bin/pylint-3"
+MAXFD = 1024
+
 def close_filedescriptors():
-		# determine maximal file descriptor limit
-		maxfd = resource.getrlimit(resource.RLIMIT_NOFILE)[1]
-		if (maxfd == resource.RLIM_INFINITY):
-		    maxfd = MAXFD
+    # determine maximal file descriptor limit
+    maxfd = resource.getrlimit(resource.RLIMIT_NOFILE)[1]
+    if maxfd == resource.RLIM_INFINITY:
+        maxfd = MAXFD
 
     # close all file descriptors left over from the parent process
     # which didn't properly close them when executing this.
-		for fd in range(3, maxfd):
-		    try:
-		        os.close(fd)
-		    except OSError:
-		        sys.stderr.write("error closing file: (%d) %s\n" % (e.errno, e.strerror))
-		        pass
+    for fd in range(6, maxfd):
+        try:
+            os.close(fd)
+        except OSError:
+            pass
 
+close_filedescriptors()
 
 command = [ REAL_EXEC ] + sys.argv
-subprocess.run(command, timeout=MAX_LIFE_SECONDS)
 
+subprocess.run(command, timeout=MAX_LIFE_SECONDS)
